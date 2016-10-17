fd6f3661f04409d3cac147ac7d7fe59791147815
Ryan McGee <Ryan McGee@MV0411095262J10.mv.scps.k12.va.us>
2016-10-17 14:13:33 -0400

    Continued work on the Vision Processor

--- src/communications/VisionProcessor.java
+++ src/communications/VisionProcessor.java
@@ -7,15 +7,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class VisionProcessor {
+	/**
+	 * Starts the server wait for the vision processing client to connect.
+	 * @author Ryan McGee
+	 * 
+	 * @param recievePort The port that will recieve data from the client.
+	 * @param sendPort	The port that will send data to the client.
+	 */
+	SearchForClient a;
+	public void startServer(int recievePort, int sendPort){
+		a = new SearchForClient(recievePort, sendPort);
+		a.start();
+	}
+	
+	public void requestProcessedImage(){
+		a.requestProcessedImage = true;
+	}
+	
 	
 	
 	static class SearchForClient extends Thread{
 		int recievePort;
 		int sendPort;
+		
+		boolean requestProcessedImage = false;
+	
 		public SearchForClient(int recievePort, int sendPort){
 			this.recievePort = recievePort;
 			this.sendPort = sendPort;
 		}
+		
 		public void run(){
 			try {
 				ServerSocket recieveListener = new ServerSocket(recievePort);
@@ -26,6 +47,13 @@ public class VisionProcessor {
 				
 				OutputStream os = sendSocket.getOutputStream();
 				InputStream is = recieveSocket.getInputStream();
+				
+				while(true){
+					if(requestProcessedImage = true){
+						
+						requestProcessedImage = false;
+					}
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
