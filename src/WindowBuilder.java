03acb57407a78eebf93097ed9cc4eb73c09d61be
Ryan McGee <Ryan McGee@MV0411095262J10.mv.scps.k12.va.us>
2016-10-27 14:14:10 -0400

    Started work on order of operations for the threshold utility.

--- src/WindowBuilder.java
+++ src/WindowBuilder.java
@@ -60,69 +60,69 @@ public class WindowBuilder extends JFrame {
 		operations[1].setBounds(151, 41, 93, 20);
 		contentPane.add(operations[1]);
 		
-		operations[2] = new JLabel("Operation 2");
-		operations[2].setBounds(151, 11, 67, 14);
-		contentPane.add(operations[2]);
+		JLabel lblOperation_1 = new JLabel("Operation 2");
+		lblOperation_1.setBounds(151, 11, 67, 14);
+		contentPane.add(lblOperation_1);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(277, 41, 93, 20);
-		contentPane.add(comboBox_2);
+		operations[2] = new JComboBox();
+		operations[2].setBounds(277, 41, 93, 20);
+		contentPane.add(operations[2]);
 		
 		JLabel lblOperation_2 = new JLabel("Operation 3\r\n");
 		lblOperation_2.setBounds(277, 11, 67, 14);
 		contentPane.add(lblOperation_2);
 		
-		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setBounds(407, 41, 93, 20);
-		contentPane.add(comboBox_3);
+		operations[3] = new JComboBox();
+		operations[3].setBounds(407, 41, 93, 20);
+		contentPane.add(operations[3]);
 		
 		JLabel lblOperation_3 = new JLabel("Operation 4");
 		lblOperation_3.setBounds(407, 11, 67, 14);
 		contentPane.add(lblOperation_3);
 		
-		JComboBox comboBox_4 = new JComboBox();
-		comboBox_4.setBounds(528, 41, 93, 20);
-		contentPane.add(comboBox_4);
+		operations[4] = new JComboBox();
+		operations[4].setBounds(528, 41, 93, 20);
+		contentPane.add(operations[4]);
 		
 		JLabel lblOperation_4 = new JLabel("Operation 5");
 		lblOperation_4.setBounds(528, 11, 67, 14);
 		contentPane.add(lblOperation_4);
 		
-		JComboBox comboBox_5 = new JComboBox();
-		comboBox_5.setBounds(22, 148, 93, 20);
-		contentPane.add(comboBox_5);
+		operations[5] = new JComboBox();
+		operations[5].setBounds(22, 148, 93, 20);
+		contentPane.add(operations[5]);
 		
 		JLabel lblOperation_5 = new JLabel("Operation 6");
 		lblOperation_5.setBounds(22, 118, 67, 14);
 		contentPane.add(lblOperation_5);
 		
-		JComboBox comboBox_6 = new JComboBox();
-		comboBox_6.setBounds(151, 148, 93, 20);
-		contentPane.add(comboBox_6);
+		operations[6] = new JComboBox();
+		operations[6].setBounds(151, 148, 93, 20);
+		contentPane.add(operations[6]);
 		
 		JLabel lblOperation_6 = new JLabel("Operation 7");
 		lblOperation_6.setBounds(151, 118, 67, 14);
 		contentPane.add(lblOperation_6);
 		
-		JComboBox comboBox_7 = new JComboBox();
-		comboBox_7.setBounds(277, 148, 93, 20);
-		contentPane.add(comboBox_7);
+		operations[7] = new JComboBox();
+		operations[7].setBounds(277, 148, 93, 20);
+		contentPane.add(operations[7]);
 		
 		JLabel lblOperation_7 = new JLabel("Operation 8");
 		lblOperation_7.setBounds(277, 118, 67, 14);
 		contentPane.add(lblOperation_7);
 		
-		JComboBox comboBox_8 = new JComboBox();
-		comboBox_8.setBounds(407, 148, 93, 20);
-		contentPane.add(comboBox_8);
+		operations[8] = new JComboBox();
+		operations[8].setBounds(407, 148, 93, 20);
+		contentPane.add(operations[8]);
 		
 		JLabel lblOperation_8 = new JLabel("Operation 9");
 		lblOperation_8.setBounds(407, 118, 67, 14);
 		contentPane.add(lblOperation_8);
 		
-		JComboBox comboBox_9 = new JComboBox();
-		comboBox_9.setBounds(528, 148, 93, 20);
-		contentPane.add(comboBox_9);
+		operations[9] = new JComboBox();
+		operations[9].setBounds(528, 148, 93, 20);
+		contentPane.add(operations[9]);
 		
 		JLabel lblOperation_9 = new JLabel("Operation 10");
 		lblOperation_9.setBounds(528, 118, 67, 14);
