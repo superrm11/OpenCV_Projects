3e91c6683aaf08d021f00e024e3c3724129e7e0b
mcgeeryana <mcgeeryana@MV0411095262J10.mv.scps.k12.va.us>
2017-03-10 10:36:43 -0500

    Made a meager attempt at relative coordinates; no time I guess...

--- src/visionUtility/ThresholdWindows.java
+++ src/visionUtility/ThresholdWindows.java
@@ -46,7 +46,6 @@ public class ThresholdWindows implements OperationWindows
 		createSlideBarLowerBound();
 		createSlideBarUpperBound();
 		this.operationIndex = operationIndex;
-
 	}
 
 	public int getOperationsIndex()
